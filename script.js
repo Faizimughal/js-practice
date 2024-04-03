94bb5029abad37935e3939e28a81212ccab953b5
Faizimughal <162406605+Faizimughal@users.noreply.github.com>
2024-04-03 09:44:16 +0500

    js practice

---
+++ script.js
@@ -0,0 +1,50 @@
+// let a=5
+// let b=10
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// document.write(a+b);
+// function addition(a , b)
+// {
+//    console.log(a+b) 
+// }
+// function subtraction(a , b)
+// {
+//    console.log(a-b) 
+// }
+// function multiplication(a , b)
+// {
+//    console.log(a*b) 
+// }
+// function division(a , b)
+// {
+//    console.log(a/b) 
+// }
+// function remainder(a , b)
+// {
+//    console.log(a%b) 
+// }
+// console.log(subtraction(20,30))
+// console.log(addition(20,30))
+// console.log(multiplication(20,30))
+// console.log(division(20,30))
+// console.log(remainder(20,30))
+// function quadratic(a , b , c){
+//      console.log(-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
+// }
+//     console.log (quadratic(20,30,2))
+// function circumference(r ,d){
+//     if (r) {
+//         return 2 * Math.PI * r
+//     }
+//     else if (d){
+//         return 2 * Math.PI *(d/2)
+//     }
+//     else {
+//         return "enter some value"
+//     }
+// }
+// document.write(circumference(2))
+function area
